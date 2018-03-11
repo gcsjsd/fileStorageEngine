971800acab659eed2096d72c34933003966e77fd
zhaowei <zjufisher@gmail.com>
2018-03-11 07:17:45 -0700

    modify code format

--- SFArchive.cpp
+++ SFArchive.cpp
@@ -6,64 +6,57 @@
 #include <vector>
 #include <stdio.h>
 SFArchive::SFArchive(std::string archiveName) {
+  this->archiveName = archiveName;
+  this->archive.open(archiveName,std::ios::out|std::ios::binary|std::ios::in);
+  bool exist = true; // the .bin file has existed
+  // if archive doesnt exist, create the archive
+  if (this->archive.tellg() == -1) {
+    std::cout << "Creating archive..." << archiveName << std::endl;
+    this->archive.close();
+    std::ofstream archive1;
+    archive1.open(archiveName, std::ios::out|std::ios::app);
+    this->initHeader(archive1); //initialize the header of the file
+    archive1.close();
+    exist = false;
+    std::cout << "Creating archive done." << std::endl;
+  }
 
-    //version and compile time
-    //version_number = 0.0;
-    //build_time = __DATE__;
-
-    //open create the arch.bin if it exists
-    this->archiveName = archiveName;
-    this->archive.open(archiveName,std::ios::out|std::ios::binary|std::ios::in);
-    bool exist = true; // the .bin file has existed
-    // if .bin doesnt exist, create the file
-    if (this->archive.tellg() == -1) {
-        std::cout << "Creating archive..." << archiveName << std::endl;
-        this->archive.close();
-        std::ofstream archive1;
-        archive1.open(archiveName, std::ios::out|std::ios::app);
-        this->initHeader(archive1); //initialize the header of the file
-        archive1.close();
-        exist = false;
-        std::cout << "Creating archive done." << std::endl;
-    }
-
-    // if just created, open this file again.
-    if (!exist) {
-        this->archive.open(archiveName, std::ios::out|std::ios::binary|std::ios::in);
-    }
-
+  // if just created, open this file again.
+  if (!exist) {
+    this->archive.open(archiveName, std::ios::out|std::ios::binary|std::ios::in);
+  }
 }
 
 void SFArchive::initHeader(std::ofstream &file) {
-    block_i blocks[maxFileNumber]; // current we can write 20 files at most
-    for(int i=0;i<maxFileNumber;i++){
-        blocks[i].type = AVA;
-        for(int j=0; j<name_size; j++){
-            blocks[i].name[j] = 'x';
-        }
-        blocks[i].size = 0;
-
-        blocks[i].day = 1;
-        blocks[i].month = 1;
-        blocks[i].year = 2000;
-        for (int j = 0; j < 100; j++) {
-          blocks[i].chunks[j] = -1;
-        }
+  block_i blocks[maxFileNumber]; // current we can write 20 files at most
+  for(int i=0;i<maxFileNumber;i++){
+    blocks[i].type = AVA;
+    for(int j=0; j<name_size; j++){
+      blocks[i].name[j] = 'x';
     }
+    blocks[i].size = 0;
 
-    file.write((char*)&blocks, sizeof(blocks));
+    blocks[i].day = 1;
+    blocks[i].month = 1;
+    blocks[i].year = 2000;
+    for (int j = 0; j < 100; j++) {
+      blocks[i].chunks[j] = -1;
+    }
+  }
+
+  file.write((char*)&blocks, sizeof(blocks));
 
 }
 
 SFArchive::~SFArchive() {
-    this->archive.close();
+  this->archive.close();
 }
 SFArchive& SFArchive::add(std::string name) {
-    /* 0.Init SFHeader header.
-     * 1.open the file, create block for this file.
-     * 2.addFileHeader, all archive header operation is done.
-     * 3.call SFile.writeArchive(archive, vector<int>chunks, file)
-     */
+  /* 0.Init SFHeader header.
+   * 1.open the file, create block for this file.
+   * 2.addFileHeader, all archive header operation is done.
+   * 3.call SFile.writeArchive(archive, vector<int>chunks, file)
+   */
 	SFHeader header;
 	header.readHeader(this->archive);
   block_i block;
@@ -115,83 +108,83 @@ SFArchive& SFArchive::add(std::string name) {
 }
 
 SFArchive& SFArchive::del(std::string name) {
-    /* 0.Init SFHeader header
-     * 1.call header.delFileHeader().
-     * 2.Things are done
-     */
-   SFHeader header;
-   header.readHeader(this->archive);
-   bool needDel = header.delFileHeader(name, this->archive);
-   if (needDel) {
-     std::string newName = this->archiveName + ".temp";
-     std::ofstream initStream;
-     initStream.open(newName, std::ios::out | std::ios::app);
-     initHeader(initStream);
-     initStream.close();
-     std::fstream newStream;
-     newStream.open(newName, std::ios::out|std::ios::binary|std::ios::in);
-     SFHeader newHeader;
-     newHeader.readHeader(newStream);
-     header.update(this->archive, newStream, newHeader);
-     this->archive.close();
-     newStream.close();
+  /* 0.Init SFHeader header
+   * 1.call header.delFileHeader().
+   * 2.Things are done
+   */
+  SFHeader header;
+  header.readHeader(this->archive);
+  bool needDel = header.delFileHeader(name, this->archive);
+  if (needDel) {
+    std::string newName = this->archiveName + ".temp";
+    std::ofstream initStream;
+    initStream.open(newName, std::ios::out | std::ios::app);
+    initHeader(initStream);
+    initStream.close();
+    std::fstream newStream;
+    newStream.open(newName, std::ios::out|std::ios::binary|std::ios::in);
+    SFHeader newHeader;
+    newHeader.readHeader(newStream);
+    header.update(this->archive, newStream, newHeader);
+    this->archive.close();
+    newStream.close();
 
-     if(remove(archiveName.c_str()) != 0 )
-       perror( "Error deleting file\n" );
+    if(remove(archiveName.c_str()) != 0 )
+      perror( "Error deleting file\n" );
 
-     int ret;
-     const char* oldname = newName.c_str();
-     const char* newname = this->archiveName.c_str();
+    int ret;
+    const char* oldname = newName.c_str();
+    const char* newname = this->archiveName.c_str();
 
-     ret = rename(oldname, newname);
+    ret = rename(oldname, newname);
 
-     if(ret != 0)
+    if(ret != 0)
       printf("Error: unable to rename the file\n");
 
-   }
-   return *this;
+  }
+  return *this;
 }
 
 SFArchive& SFArchive::extract(std::string name) {
-    /* 0.Init SFHeader header
-     * 1.ofstream open file
-     * 2.call header.getFile()
-     * 3.call header.getFileSize()
-     * 4.call SFile.readArchive(archive, vector<int>chunks, file, size)
-     */
+  /* 0.Init SFHeader header
+   * 1.ofstream open file
+   * 2.call header.getFile()
+   * 3.call header.getFileSize()
+   * 4.call SFile.readArchive(archive, vector<int>chunks, file, size)
+   */
 
-    SFHeader header;
-    header.readHeader(this->archive);
+  SFHeader header;
+  header.readHeader(this->archive);
 
-    //check if the file exists, read the size and the chunk;
-    std::vector<int> chunks;
-    //int atype = header.typeI2S(type);
-    chunks = header.getFile(name);
-    if(chunks.size()==0){
-        std::cout<<"this file doesn't exist or has been deleted."<<std::endl;
-        return *this;
-    }
-
-    int fileSize = header.getFileSize(name);
-    
-    SFile sfile;
-    std::ofstream extractFile(name);
-    sfile.readArchive(this->archive, chunks, extractFile, fileSize);
-    extractFile.close();
-    std::cout << "extract done" << std::endl;
+  //check if the file exists, read the size and the chunk;
+  std::vector<int> chunks;
+  //int atype = header.typeI2S(type);
+  chunks = header.getFile(name);
+  if(chunks.size()==0){
+    std::cout<<"this file doesn't exist or has been deleted."<<std::endl;
     return *this;
+  }
+
+  int fileSize = header.getFileSize(name);
+    
+  SFile sfile;
+  std::ofstream extractFile(name);
+  sfile.readArchive(this->archive, chunks, extractFile, fileSize);
+  extractFile.close();
+  std::cout << "extract done" << std::endl;
+  return *this;
 
 }
 
 void SFArchive::list(std::string content) {
-    /* 0.Init SFHeader header
-     * 1.call header.listFiles(content)
-     */
-    //read header
-    SFHeader header;
-    header.readHeader(this->archive);
-    header.listFiles(content);
-    return;
+  /* 0.Init SFHeader header
+   * 1.call header.listFiles(content)
+   */
+  //read header
+  SFHeader header;
+  header.readHeader(this->archive);
+  header.listFiles(content);
+  return;
 
 }
 
@@ -208,16 +201,14 @@ void SFArchive::list(){
 void SFArchive::search(std::string content) {
     /*1.call find_txt_Files()
      *2.call  header.getFile()
-	 *3.found->call Header.list()
+	   *3.found->call Header.list()
      */
 	SFHeader header;
-	//bool found = false;
 	header.readHeader(this->archive);
 	std::vector<std::string> TXTFiles;
 	TXTFiles = header.find_txt_Files();
   std::vector<std::string> foundFiles;
 	for (std::string txtName : TXTFiles) {
-    //std::cout << "searching in " << txtName << std::endl;
 		std::vector<int> txtFile = header.getFile(txtName);
     int txtSize = header.getFileSize(txtName);
     char* bufferTXT = new char[txtSize];
@@ -228,13 +219,6 @@ void SFArchive::search(std::string content) {
       txtSize -= readSize;
       this->archive.read(&(bufferTXT[readPos]), readSize);
       readPos += readSize;
-      /*
-			if (txt.find(content) != std::string::npos) {
-				header.listFiles(txtName);
-				found = true;
-				break;
-			}
-       */
 		}
     std::string tmpTXT(bufferTXT);
     size_t pos = tmpTXT.find(content);
--- SFHeader.cpp
+++ SFHeader.cpp
@@ -209,19 +209,7 @@ void SFHeader::listFiles(std::string s) {
 
 void SFHeader::listFiles() {
     for (int i = 0; i < sizeof(blocks)/sizeof(block_i); i++) {
-        //std::cout << "type: " << this->blocks[i].type << std::endl;
         if (this->blocks[i].type != AVA) {
-          /*
-          std::cout << this->blocks[i].name << " " << this->blocks[i].size <<"byte " << this->blocks[i].day << " " << this->blocks[i].month << " " << this->blocks[i].year << std::endl;
-          int totalChunks = 0;
-          while (totalChunks < sizeof(this->blocks[i].chunks) / sizeof(short) && this->blocks[i].chunks[totalChunks] != -1) {
-            totalChunks++;
-          }
-          for (int j = 0; j < totalChunks; j++) {
-            std::cout << "chunks num : " << this->blocks[i].chunks[j] << " ";
-          }
-          std::cout << std::endl;
-            */
             std::cout << this->blocks[i].name << " " << this->blocks[i].size <<"byte " << this->blocks[i].month << "/" << this->blocks[i].day << "/"  << this->blocks[i].year << std::endl;
 
         }
